279c6b546a57218c1ed6234abe624c62e9f768e6
Paulo César Conduru <133069658+pcconduru@users.noreply.github.com>
2024-07-22 02:39:42 -0300

    Create exr_pathway1-E3.kt

---
+++ exr_pathway1-E3.kt
@@ -0,0 +1,10 @@
+//Coreção de Valor para Váriavel 
+fun main() {
+    var discountPercentage: Int = 0
+    var offer: String = ""
+    val item = "Google Chromecast"
+    discountPercentage = 20
+    offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
+    
+    println(offer)
+}
