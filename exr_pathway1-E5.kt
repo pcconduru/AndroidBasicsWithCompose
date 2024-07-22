26df7837e32d6c24267563838dc74a8d908227de
Paulo César Conduru <133069658+pcconduru@users.noreply.github.com>
2024-07-22 02:45:10 -0300

    Create exr_pathway1-E5.kt

---
+++ exr_pathway1-E5.kt
@@ -0,0 +1,7 @@
+//Corrigindo Operação errada que ultilizava string como base
+fun main() {
+    val baseSalary = 5000
+    val bonusAmount = 1000
+    val totalSalary = baseSalary + bonusAmount
+    println("Congratulations for your bonus! You will receive a total of $totalSalary.")
+}
