551945309e7c9a0ca96402e187061729ee940fda
Paulo César Conduru <133069658+pcconduru@users.noreply.github.com>
2024-07-22 02:41:49 -0300

    Create exr_pathway1-E4.kt

---
+++ exr_pathway1-E4.kt
@@ -0,0 +1,7 @@
+//Corrigindo String para Int
+fun main() {
+    val numberOfAdults = 20
+    val numberOfKids = 30
+    val total = numberOfAdults + numberOfKids
+    println("The total party size is: $total")
+}
