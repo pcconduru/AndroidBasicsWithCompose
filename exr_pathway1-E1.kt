40b702ead40d8579910a645b16efd67e667f2624
Paulo César Conduru <133069658+pcconduru@users.noreply.github.com>
2024-07-22 02:30:00 -0300

    Create exr_pathway1-E1.kt

---
+++ exr_pathway1-E1.kt
@@ -0,0 +1,6 @@
+fun main() {
+    println("Use the val keyword when the value doesn't change.")
+    println("Use the var keyword when the value can change.")
+    println("When you define a function, you define the parameters that can be passed to it.")
+    println("When you call a function, you pass arguments for the parameters.")
+}
